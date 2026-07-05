5dd85459a81e9706c4fddbea2b2e2a14abe37d37
Pawan Kumar Shukla <108550691+pawanpks2018@users.noreply.github.com>
2026-07-05 20:27:50 +0530

    Time: 323 ms (44.73%), Space: 10.6 MB (65.77%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,44 +1,40 @@
-class Solution {
-public://code perfect hai tle aayega dp se solve hoga
-    int l, m, n;
-    vector<vector<int>> directions{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    bool find(vector<vector<char>>& board, int i, int j, string &word, int idx) {
-        if(idx >= l)
-            return true;
-        
-        if(i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[idx])
-            return false;
-        
-        char temp = board[i][j];
-        board[i][j] = '$';
-        
-        for(auto& dir : directions) {
-            int i_ = i + dir[0];
-            int j_ = j + dir[1];
-            
-            if(find(board, i_, j_, word, idx+1))
-                return true;
-        }
-        
-        board[i][j] = temp;
-        return false;
-    }
-    
+class Solution {// code sahi hai lekin ye dynamic method se solve hoga
+public:
+   int m,n,l;
+   vector<vector<int>> directions={{0,1},{0,-1},{1,0},{-1,0}};
+
     bool exist(vector<vector<char>>& board, string word) {
-        m = board.size();
-        n = board[0].size();
-        l = word.length();
-        if(m*n < l)
-            return false;
-        
-        for(int i = 0; i<m; i++) {
-            for(int j = 0; j<n; j++) {
-                if(board[i][j] == word[0] && find(board, i, j, word, 0)) {
-                    return true;
-                }
+        m=board.size();
+
+        n=board[0].size(); 
+        l=word.size();
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]==word[0] && find(word,0,i,j,board) ) return true;
             }
         }
-        
+       return false;
+    }
+
+    bool find(string &word,int index,int i ,int j,vector<vector<char>>&board){
+        if(index==l){
+            return true;
+        }
+
+        if(i<0||i>=m||j<0||j>=n|| board[i][j]=='$'|| board[i][j] != word[index]) return false;
+
+        char temp=board[i][j];
+        board[i][j]='$';
+        for(auto &dir:directions){
+            int new_i=i+dir[0];
+
+            int new_j=j+dir[1];
+            if(find(word,index+1,new_i,new_j,board)) return true;
+        }
+        board[i][j]=temp;
         return false;
+
+
     }
 };
