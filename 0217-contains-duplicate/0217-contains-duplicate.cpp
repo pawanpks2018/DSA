bdc65b8048d7ecec4f399f0106b4ebd3186ab84f
Pawan Kumar Shukla <108550691+pawanpks2018@users.noreply.github.com>
2026-07-14 10:24:28 +0530

    Time: 65 ms (34.66%), Space: 111.3 MB (18.83%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set s(nums.begin(),nums.end());
-        return nums.size() !=s.size();
+        unordered_set<int>s;
 
-        
+        for(auto i :nums){
+            if(s.find(i)==s.end()){ s.insert(i);
+        }
+        else
+        {
+
+            return true;
+        }
+
+        }
+        return false;
         
     }
 };
